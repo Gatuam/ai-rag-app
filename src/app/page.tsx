3b078757cf531319ed8dc290fec7e4999f2552f1
EK GAUTHAM <sggtm42@gmail.com>
2025-09-26 18:40:56 +0700

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/global/mode-toggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div>
+    <ModeToggle/>
+  </div>
+  );
+}
