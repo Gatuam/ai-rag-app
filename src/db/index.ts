1065150fa8bb161d6d10bea7ad984743d3ac8967
EK GAUTHAM <sggtm42@gmail.com>
2025-10-09 10:11:27 +0700

    db,schema

---
+++ src/db/index.ts
@@ -0,0 +1,8 @@
+// Make sure to install the 'pg' package
+import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema";
+
+export const db = drizzle(process.env.DATABASE_URL!,
+     { schema });
+
+const result = await db.execute("select 1");
